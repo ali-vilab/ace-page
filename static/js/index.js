ceb348cced723cc12a933106fd0a9c8210236731
Jingfeng727 <zhjf@zju.edu.cn>
2024-09-30 18:19:46 +0800

    slow video change

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 			loop: true,
 			infinite: true,
 			autoplay: true,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 15000,
     }
 
 		// Initialize all div with carousel class
